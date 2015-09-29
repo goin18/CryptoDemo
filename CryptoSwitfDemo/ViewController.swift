0e98ddbf2e289c4358ee47a9e7bc4ae066d490a0
goin18 <markobudal@gmail.com>
2015-09-29 13:22:49 +0200

    Initial Commit

---
+++ CryptoSwitfDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CryptoSwitfDemo
+//
+//  Created by Marko Budal on 29/09/15.
+//  Copyright © 2015 Marko Budal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ CryptoSwitfDemoTests/CryptoSwitfDemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  CryptoSwitfDemoTests.swift
+//  CryptoSwitfDemoTests
+//
+//  Created by Marko Budal on 29/09/15.
+//  Copyright © 2015 Marko Budal. All rights reserved.
+//
+
+import XCTest
+@testable import CryptoSwitfDemo
+
+class CryptoSwitfDemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
