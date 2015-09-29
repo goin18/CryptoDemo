aa091d719f5a17d9d5ad711236a354c59d028f66
goin18 <markobudal@gmail.com>
2015-09-29 14:03:40 +0200

    crc32

--- CryptoSwitfDemo/ViewController.swift
+++ CryptoSwitfDemo/ViewController.swift
@@ -7,12 +7,36 @@
 //
 
 import UIKit
+import CryptoSwift
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let data:NSData = NSData(bytes: [49, 50, 51] as [UInt8], length: 3)
+        
+        print("md5: \(data.md5()!.toHexString())")
+        print("sha1: \(data.sha1()!.toHexString())")
+        print("sha224: \(data.sha224()!.toHexString())")
+        print("sha256: \(data.sha256()!.toHexString())")
+        print("sha334: \(data.sha384()!.toHexString())")
+        print("sha512: \(data.sha512()!.toHexString())")
+        print("crc32: \(data.crc32()!.toHexString())")
+        
+        print("Data: \(data.toHexString())")
+        let crc32 = data.crc32()
+        print("Crc32: \(crc32)")
+        let d = crc32?.arrayOfBytes()
+        print("Array: \(d)")
+        
+
+        var test = NSData(bytes: [0x10, 0x55, 0x01, 0x00, 0x55, 0x23, 0x06,0x94,0x00, 0xE4,0x10,0x03,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00 ] as [UInt8], length: 28)
+        //105501005523069400E4100300000000000000000000000000000000
+        print(test.crc32()?.toHexString())
+        
+
     }
 
     override func didReceiveMemoryWarning() {
---
+++ Pods/Target Support Files/Pods-CryptoSwitfDemo/Pods-CryptoSwitfDemo-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_CryptoSwitfDemoVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_CryptoSwitfDemoVersionString[];
+
